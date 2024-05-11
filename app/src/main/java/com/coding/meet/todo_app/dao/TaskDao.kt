e21253d7421f1f038f6e4300c188d67260f824c0
Shashintha27 <shashintha.lakshan27@gmail.com>
2024-05-12 00:39:36 +0530

    Create dao

--- app/src/main/java/com/coding/meet/todo_app/dao/TaskDao.kt
+++ app/src/main/java/com/coding/meet/todo_app/dao/TaskDao.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TaskDao {
 
-
     @Query("""SELECT * FROM Task ORDER BY
         CASE WHEN :isAsc = 1 THEN taskTitle END ASC, 
         CASE WHEN :isAsc = 0 THEN taskTitle END DESC""")
@@ -20,7 +19,6 @@ interface TaskDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: Task): Long
-
 
     // First way
     @Delete
@@ -35,10 +33,8 @@ interface TaskDao {
     @Update
     suspend fun updateTask(task: Task): Int
 
-
     @Query("UPDATE Task SET taskTitle=:title, description = :description WHERE taskId = :taskId")
     suspend fun updateTaskPaticularField(taskId:String,title:String,description:String): Int
-
 
     @Query("SELECT * FROM Task WHERE taskTitle LIKE :query ORDER BY date DESC")
     fun searchTaskList(query: String) : Flow<List<Task>>
